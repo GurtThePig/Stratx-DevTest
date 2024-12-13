022a9bf7d049a4802f1190ce8d6b67b84866584d
Gurty <synergysypher@gmail.com>
2024-12-13 19:23:19 +0700

    Update Ability.lua

--- TDS/TDSTools/Functions/Ability.lua
+++ TDS/TDSTools/Functions/Ability.lua
@@ -18,27 +18,43 @@ return function(self, p1)
     if not CheckPlace() then
         return
     end
+    local CurrentCount = StratXLibrary.CurrentCount
     SetActionInfo("Ability","Total")
     task.spawn(function()
-        if not TimeWaveWait(Wave, Min, Sec, InWave, tableinfo["Debug"]) then
+        local TimerCheck = TimeWaveWait(Wave, Min, Sec, InWave, tableinfo["Debug"])
+        if not TimerCheck then
             return
         end
-        if not TowersCheckHandler(Tower) then
-            return
-        end
-        if Ability == "Call Of Arms" and TowersContained[Tower].AutoChain then
-            local TowerType = GetTypeIndex(tableinfo["TypeIndex"],Tower)
-            SetActionInfo("Ability")
-            ConsoleInfo("Skipped Ability (AutoChain Enabled) On Tower Index: "..Tower..", Type: \""..TowerType.."\", (Wave "..Wave..", Min: "..Min..", Sec: "..Sec..", InBetween: "..tostring(InWave)..")")
-            return
-        end
-        RemoteFunction:InvokeServer("Troops","Abilities","Activate",{
-            ["Troop"] = TowersContained[Tower].Instance,
-            ["Name"] = Ability,
-            ["Data"] = Data,
-        })
+        local CheckAbilityUsed, SkipCheck
+        task.delay(50, function()
+            SkipCheck = true
+        end)
+        repeat
+            if not TowersCheckHandler(Tower) then
+                prints("End Ability",Wave, Min, Sec, InWave)
+                return
+            end
+            if Ability == "Call Of Arms" and TowersContained[Tower].AutoChain then
+                local TowerType = GetTypeIndex(tableinfo["TypeIndex"],Tower)
+                SetActionInfo("Ability")
+                ConsoleInfo("Skipped Ability (AutoChain Enabled) On Tower Index: "..Tower..", Type: \""..TowerType.."\", (Wave "..Wave..", Min: "..Min..", Sec: "..Sec..", InBetween: "..tostring(InWave)..")")
+                return
+            end
+            CheckAbilityUsed = RemoteFunction:InvokeServer("Troops","Abilities","Activate",{
+                ["Troop"] = TowersContained[Tower].Instance,
+                ["Name"] = Ability,
+                ["Data"] = Data,
+            })
+        until CheckAbilityUsed or SkipCheck
         local TowerType = GetTypeIndex(tableinfo["TypeIndex"],Tower)
+        if CurrentCount ~= StratXLibrary.RestartCount then
+            return
+        end
+        if SkipCheck and not CheckAbilityUsed then
+            ConsoleError("Failed To Use Ability On Tower Index: "..Tower..", Type: \""..TowerType.."\", (Wave "..Wave..", Min: "..Min..", Sec: "..Sec..", InBetween: "..tostring(InWave)..") CheckAbilityUsed: "..tostring(CheckAbilityUsed)..", SkipCheck: "..tostring(SkipCheck))
+            return
+        end
         SetActionInfo("Ability")
-        ConsoleInfo("Used Ability On Tower Index: "..Tower..", Type: \""..TowerType.."\", (Wave "..Wave..", Min: "..Min..", Sec: "..Sec..", InBetween: "..tostring(InWave)..")")
+        ConsoleInfo("Used Ability On Tower Index: "..Tower..", Type: \""..TowerType.."\", (Wave "..Wave..", Min: "..Min..", Sec: "..Sec..", InBetween: "..tostring(InWave)..") CheckAbilityUsed: "..tostring(CheckAbilityUsed)..", SkipCheck: "..tostring(SkipCheck))
     end)
 end
